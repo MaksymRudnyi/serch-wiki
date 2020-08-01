f4df67b7e13674c27a0ae19a9e406208cf873b75
Maksym Rudnyi <maksym.rudnyi@fiverr.com>
2020-08-01 18:41:58 +0300

    add the first custom hook

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useEffect, useState, useRef} from "react";
 import axios from 'axios';
 
 export const useSearch = (query) => {
@@ -9,8 +9,23 @@ export const useSearch = (query) => {
         error: ''
     });
 
+    const cancelToken = useRef(null);
+
     useEffect(() => {
-        axios.get(`https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=${query}`)
+        if (query.length < 3) {
+            return;
+        }
+
+        if (cancelToken.current) {
+            cancelToken.current.cancel();
+        }
+
+
+        cancelToken.current = axios.CancelToken.source();
+
+        axios.get(`https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=${query}`, {
+            cancelToken: cancelToken.current.token
+        })
             .then(function (response) {
                 const parsedResponse = [];
 
@@ -28,11 +43,15 @@ export const useSearch = (query) => {
                 })
             })
             .catch(function (error) {
+                if (axios.isCancel(error)) {
+                    return
+                }
+
                 setState({
                     articles: [],
                     status: 'ERROR',
                     error: error
-                })
+                });
             })
     }, [query]);
 
