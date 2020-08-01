2f901a29118a6497847b63471945c5092770e88a
Maksym Rudnyi <maksym.rudnyi@fiverr.com>
2020-08-01 15:24:18 +0300

    Added autocomplete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactAutocomplete from 'react-autocomplete'
 
 function App() {
+    const [ value, setValue ] = useState();
+    const [ placeholder, setPlaceholder ] = useState();
+
     return (
-        <div className="App">
-            Hello World
-        </div>
+        <ReactAutocomplete
+            items={[
+                { id: 'foo', label: 'foo' },
+                { id: 'bar', label: 'bar' },
+                { id: 'baz', label: 'baz' },
+            ]}
+            shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
+            getItemValue={item => item.label}
+            renderItem={(item, highlighted) =>
+                <div
+                    key={item.id}
+                    style={{ backgroundColor: highlighted ? '#eee' : 'transparent'}}
+                >
+                    {item.label}
+                </div>
+            }
+            value={value}
+            onChange={e => setValue(e.target.value )}
+            onSelect={value => setValue(value)}
+        />
     )
 }
 
