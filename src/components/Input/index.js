f242688fb2be7902e37f026f29a9de03aa05984c
Maksym Rudnyi <maksym.rudnyi@fiverr.com>
2020-08-02 13:57:33 +0300

    add scss

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './styles.css'
+import './styles.scss'
 
 const Input = ({placeholder, ...rest}) => <input className="input-field" placeholder={placeholder} {...rest}/>
 
