233ac52a0e49c8fa82b68ed00fcd7dd6dc122b23
Maksym Rudnyi <maksym.rudnyi@fiverr.com>
2020-10-05 22:44:31 +0300

    Container component

---
+++ src/components/Container/index.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { mount } from 'enzyme'
+
+import Container from '.';
+
+jest.mock('../../hooks', () => ({
+    useDebounce: jest.fn(() => ({})),
+    useSearch: jest.fn(() => ({articles: []})),
+    useSearchForm: jest.fn(() => ({searchValue: 'test', onSearchChange: jest.fn()})),
+}));
+
+const render = (props, children) => mount(<Container {...props}>{children}</Container>);
+
+const Children = () => <div>Test</div>;
+
+describe('Container component', () => {
+    let sut;
+    let props;
+    let children = ({searchValue, onSearchChange, articles}) => <Children
+        searchValue={searchValue}
+        onSearchChange={onSearchChange}
+        articles={articles}
+    />;
+
+    describe('without props', () => {
+        beforeEach(() => {
+            sut = render(props, children);
+        });
+
+        it('should match snapshot', () => {
+            expect(sut).toMatchSnapshot();
+        })
+    });
+
+    describe('when component is returned with props', () => {
+        beforeEach(() => {
+            sut = render(props, children);
+        });
+
+        it('should return searchValue in Children component', () => {
+            const { searchValue } = sut.find('Children').props();
+            expect(searchValue).toBe('test');
+        })
+    });
+});
