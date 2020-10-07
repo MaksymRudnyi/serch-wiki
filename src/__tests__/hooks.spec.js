906e8bb92bd37d20b52d12b0d5d59a616d3f46dc
Maksym Rudnyi <maksym.rudnyi@fiverr.com>
2020-10-07 21:32:15 +0300

    useDebounce hook

--- src/__tests__/hooks.spec.js
+++ src/__tests__/hooks.spec.js
@@ -1,6 +1,8 @@
-import { renderHook, act } from 'react-hooks-testing-library';
+import { renderHook, act } from '@testing-library/react-hooks';
 
-import {useSearchForm} from '../hooks';
+import { useSearchForm, useDebounce } from '../hooks';
+
+jest.useFakeTimers();
 
 describe('useSearchForm hook', () => {
     let event1;
@@ -31,4 +33,37 @@ describe('useSearchForm hook', () => {
 
         expect(result.current.searchValue).toEqual('updated event');
     })
+});
+
+describe('useDebounce hook', () => {
+    it('should update value after specified delay', () => {
+        const val1 = '1';
+        const { result } = renderHook(() => useDebounce(val1, 500));
+
+        expect(result.current).toEqual(val1);
+        jest.advanceTimersByTime(510);
+        expect(result.current).toBe(val1);
+    });
+
+    it('should return the same value before timeout is reached', () => {
+        const val1 = '1';
+        const val2 = '2';
+        // const { result, rerender } = renderHook(() => useDebounce(val1, 500));
+
+        const { result, rerender } = renderHook(
+            ({ value, delay }) => useDebounce(value, delay),
+            { initialProps: { value: val1, delay: 500 } }
+        );
+
+        expect(result.current).toEqual(val1);
+        jest.advanceTimersByTime(498);
+
+        rerender({ value: val2 });
+
+        expect(result.current).toBe(val1);
+
+        jest.runAllTimers();
+
+        expect(result.current).toBe(val2);
+    });
 });
